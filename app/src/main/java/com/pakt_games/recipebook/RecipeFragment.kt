8dced3fd418869efb0b2420dc42e265cfc6fa942
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-09-09 22:22:50 +0300

    Required permissions have been requested from the user.

--- app/src/main/java/com/pakt_games/recipebook/RecipeFragment.kt
+++ app/src/main/java/com/pakt_games/recipebook/RecipeFragment.kt
@@ -1,10 +1,15 @@
 package com.pakt_games.recipebook
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.fragment_recipe.*
 
 
@@ -45,7 +50,21 @@ class RecipeFragment : Fragment() {
     }
     fun btnImageSelection(view: View)
     {
-        println("İmage Ekleme Çalışıyor")
+        activity.let {
+
+            if(ContextCompat.checkSelfPermission(it!!.applicationContext,Manifest.permission.READ_EXTERNAL_STORAGE) !=PackageManager.PERMISSION_GRANTED)
+            {
+                //Permission DENIED
+                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
+            }
+            else
+            {
+                //Permission GRANTED
+                val galleryIntent=Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galleryIntent,2)
+            }
+        }
+
     }
 
 
